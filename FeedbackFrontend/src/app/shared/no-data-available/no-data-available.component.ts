fd5ebe4d0d8e6313db11232840c9a3fc64734fa2
siddharth-garg-cs <siddharth.g@codestoresolutions.com>
2023-09-07 10:50:51 +0530

    dynamic messages in no data component

--- FeedbackFrontend/src/app/shared/no-data-available/no-data-available.component.ts
+++ FeedbackFrontend/src/app/shared/no-data-available/no-data-available.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-no-data-available',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./no-data-available.component.scss']
 })
 export class NoDataAvailableComponent {
-
+  @Input() message:string = "";
 }
