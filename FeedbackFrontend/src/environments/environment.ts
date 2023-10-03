9dbe8411171ed227e076bc6872dedbba3253487a
akshara <akshara@codestoresolutions.com>
2023-10-03 17:10:23 +0530

    update: environment file api url config

--- FeedbackFrontend/src/environments/environment.ts
+++ FeedbackFrontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    feedbackApiUrl: 'https://feedbackbackend-dev.azurewebsites.net/api/v1/'
+    feedbackApiUrl: 'https://feedback-dev-service.azurewebsites.net/api/v1/'
 };
